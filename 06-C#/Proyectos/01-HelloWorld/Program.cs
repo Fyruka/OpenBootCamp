04633e2616adbf7036253bc63cf0f77e6f23c279
Samuel-Galindo <samuelgalindop@gmail.com>
2022-07-05 19:44:55 +0200

    C# Ejercicio Tema 1

--- 06-C#/Proyectos/01-HelloWorld/Program.cs
+++ 06-C#/Proyectos/01-HelloWorld/Program.cs
@@ -1,4 +1,13 @@
-﻿string nombre = "Samuel";
-
+﻿
 Console.WriteLine("Hola, mundo!");
+
+string nombre = "Samuel";
 Console.WriteLine("Hola, " + nombre);
+
+
+Console.WriteLine("Introduce tu nombre:");
+string nombre_consola = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre_consola);
+
+DateTime dateTime = DateTime.UtcNow.Date;
+Console.WriteLine(dateTime.ToString("dd/MM/yyyy"));
