d6361add31dafc533ba581c812034482acffd718
Iz Pandaa <samuelgalindop@gmail.com>
2022-07-13 18:45:59 +0200

    C# Temas & Ejercicios

---
+++ 06-C#/Proyectos/5.1-Ejercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿//Ejercicio 1 - If
+
+//Escribe un programa que:
+//  - Pida datos a un cliente: Nombre, email, cupón
+//  - Determine si un cliente tiene un cupon descuento
+//  - Muestre un precio rebajado en función del descuento
+//  - Muestre el precio de un producto sin descuento si no hay cupón
+
+//Nota: puedes poner un precio fijo de un producto o uno variable.
+
+Console.WriteLine("Escribe tu nombre: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Escribe tu email: ");
+string email = Console.ReadLine();
+
+Console.WriteLine("Escribe tu descuento: ");
+string? cupon = Console.ReadLine();
+
+
+int price = 1000;
+int finalprice = price;
+
+Console.WriteLine(cupon);
+if (!string.IsNullOrEmpty(cupon))
+{
+    finalprice = price - (price * Convert.ToInt32(cupon)) / 100;
+}
+
+Console.WriteLine($"Nombre cliente: {name} \nEmail: {email} \nEl precio que ha solicitado es: ${finalprice}");
---
+++ 06-C#/Proyectos/5.2-Ejercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ejercicio 2 - Switch
+
+//Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++. Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 1, 2, 3 o a, b, c. Presenta el resultado en consola.
+
+//Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.
+
+var lenguages = new List<string> { "C#", "Java", "C++", "JavaScript", "Python"};
+
+foreach (var (lenguage, index) in lenguages.Select((value, i) => (value, i)))
+{
+    Console.WriteLine($"{index}: {lenguage}");
+}
+
+Console.WriteLine("Selecciona un lenguaje con el num: ");
+int select = Convert.ToInt32(Console.ReadLine());
+
+if (select == 0)
+{
+    Console.WriteLine($"Has seleccionado: {lenguages[select]}");
+    Console.WriteLine($"Hola mundo! ^^");
+}
+else
+{
+    Console.WriteLine($"Has seleccionado: {lenguages[select]}");
+}
