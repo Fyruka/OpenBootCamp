d1a4a843234f4c6856f7c42b1ea741dab9e9ca2d
Iz Pandaa <samuelgalindop@gmail.com>
2022-08-29 17:17:25 +0200

    DotNET Curso & Ejercicios

---
+++ 07-DotNET/Proyectos/Practicas/2.1-University/LinqSnippets/Enterprise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSnippets
+{
+    internal class Enterprise
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Employee[] Employees { get; set; } = new Employee[0]; // Creamos una lista desde la tabla empleados. Y le creamos 1 empleado ficticio para tener un elemento en ella. 
+    }
+}
