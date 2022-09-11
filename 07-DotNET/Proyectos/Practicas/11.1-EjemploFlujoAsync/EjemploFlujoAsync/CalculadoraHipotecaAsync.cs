f844694dcf7b16d1f20ec090c81c2dd81aff9a03
Iz Pandaa <samuelgalindop@gmail.com>
2022-09-11 13:46:03 +0200

    DotNET Curso & Ejercicios

---
+++ 07-DotNET/Proyectos/Practicas/11.1-EjemploFlujoAsync/EjemploFlujoAsync/CalculadoraHipotecaAsync.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploFlujoAsync
+{
+    public static class CalculadoraHipotecaAsync
+    {
+        public static async Task<int> ObtenerAniosVidaLaboral() // En este caso la funcion la declaramos asincrona, y vamos a devolver una Task o tarea, que se va a resolver en el futuro.  
+        {
+            Console.WriteLine("\nObteniendo años de vida laboral...");
+            await Task.Delay(5000); // Esta es la tarea a realizar, por eso le decimos que await aqui. normalmente en un codigo de una API tendremos tareas como lecturas en bases de datos, y eso es lo que la funcion tiene que esperar a que termine para continuar.   
+            return new Random().Next(1, 35); 
+        }
+
+        public static async Task<bool> EsTipoContratoIndefinido()
+        {
+            Console.WriteLine("\nVerificando si el tipo de contrato es indefinido...");
+            await Task.Delay(5000);
+            return (new Random().Next(1, 10)) % 2 == 0; 
+        }
+
+        public static async Task<int> ObtenerSueldoNeto()
+        {
+            Console.WriteLine("\nObteniendo sueldo neto...");
+            await Task.Delay(5000);
+            return new Random().Next(800, 6000);
+        }
+
+        public static async Task<int> ObtenerGastosMensuales()
+        {
+            Console.WriteLine("\nObteniendo gastos mensuales...");
+            await Task.Delay(10000);
+            return new Random().Next(200, 1000);
+        }
+
+        public static bool AnalizarInformacionParaConcederHipoteca(
+           int aniosVidaLaboral,
+           bool tipoContratoEsIndefinido,
+           int sueldoNeto,
+           int gastosMensuales,
+           int cantidadSolicitada,
+           int aniosPagar)
+        {
+            Console.WriteLine("\nAnalizando informacion para conceder hipoteca...");
+
+            if (aniosVidaLaboral < 2)
+            {
+                return false;
+            }
+
+            // Obtener la cuota mensual a pagar
+            var cuota = (cantidadSolicitada / aniosPagar) / 12;
+
+            if (cuota >= sueldoNeto || cuota > (sueldoNeto / 2))
+            {
+                return false;
+            }
+
+            var porcentajeGastosSobreSueldo = ((gastosMensuales * 100) / sueldoNeto);
+
+            if (porcentajeGastosSobreSueldo > 30)
+            {
+                return false;
+            }
+
+            if ((cuota + gastosMensuales) >= sueldoNeto)
+            {
+                return false;
+            }
+
+            if (!tipoContratoEsIndefinido)
+            {
+                if ((cuota + gastosMensuales) > sueldoNeto / 3)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            // Si no entra en ninguna de las condiciones, sí que la concedemos
+            return true;
+        }
+    }
+}
---
+++ 07-DotNET/Proyectos/Practicas/11.1-EjemploFlujoAsync/EjemploFlujoAsync/Program.cs
@@ -0,0 +1,93 @@
+﻿using EjemploFlujoAsync;
+using System.Diagnostics; // Con este modulo podemos ver, analizar y calcular los tiempos de ejecucion de nuestras aplicaciones. 
+
+
+// Iniciamos un contrador de tiempo - SINCRONA
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+Console.WriteLine("\n**************************************************");
+Console.WriteLine("\nBienvenido a la Calculadora de Hipotecas Sincrona");
+Console.WriteLine("\n**************************************************");
+
+var aniosVidaLaboral = CalculadoraHipotecaSync.ObtenerAniosVidaLaboral();
+Console.WriteLine($"\nAños de Vida Laboral Obtenidos: {aniosVidaLaboral}");
+
+var esTipoContratoIndefinido = CalculadoraHipotecaSync.EsTipoContratoIndefinido();
+Console.WriteLine($"\nTipo de contrato indefinido: {esTipoContratoIndefinido}");
+
+var sueldoNeto = CalculadoraHipotecaSync.ObtenerSueldoNeto();
+Console.WriteLine($"\nSueldo netro obtenido: {sueldoNeto}€");
+
+var gastosMenusales = CalculadoraHipotecaSync.ObtenerGastosMensuales();
+Console.WriteLine($"\nGastos mensuales obtenidos: {gastosMenusales}€");
+
+var hipotecaConcedida = CalculadoraHipotecaSync.AnalizarInformacionParaConcederHipoteca(aniosVidaLaboral, esTipoContratoIndefinido, sueldoNeto, gastosMenusales, cantidadSolicitada: 5000, aniosPagar: 30);
+
+var resultado = hipotecaConcedida ? "APROBADA" : "DENEGADA";
+
+Console.WriteLine($"\nAnalisis finalizado. Su solicitud de hipoteca ha sido: {resultado}");
+
+// Paramos el contador
+stopwatch.Stop();
+
+Console.WriteLine($"\n La operacion ha durado: {stopwatch.Elapsed}");
+
+
+
+// REIniciamos un contrador de tiempo - ASINCRONA
+
+stopwatch.Restart();
+Console.WriteLine("\n****************************************************");
+Console.WriteLine("\nBienvenido a la Calculadora de Hipotecas Asincrona");
+Console.WriteLine("\n****************************************************");
+
+Task<int> aniosVidaLaboralTask = CalculadoraHipotecaAsync.ObtenerAniosVidaLaboral();
+Task<bool> esTipoContratoIndefinidoTask = CalculadoraHipotecaAsync.EsTipoContratoIndefinido();
+Task<int> sueldoNetoTask = CalculadoraHipotecaAsync.ObtenerSueldoNeto();
+Task<int> gastosMenusalesTask = CalculadoraHipotecaAsync.ObtenerGastosMensuales();
+
+var analisisHipotecaTasks = new List<Task>
+{
+    aniosVidaLaboralTask,
+    esTipoContratoIndefinidoTask,
+    sueldoNetoTask,
+    gastosMenusalesTask
+};
+
+while (analisisHipotecaTasks.Any()) // Si cualquiera de estas Task de la lista ha terminado...
+{
+    Task tareaFinalizada = await Task.WhenAny(analisisHipotecaTasks); // Esperamos de forma activa a que cualquiera de las tareas termine y nos diga cual es. Vamos obtreniendo las tareas a medida que se completen. 
+
+    if (tareaFinalizada == aniosVidaLaboralTask)
+    {
+        Console.WriteLine($"\nAños de Vida Laboral Obtenidos: {aniosVidaLaboralTask.Result}"); // Con result accedemos al resultado de la tarea. 
+    }
+    else if(tareaFinalizada == esTipoContratoIndefinidoTask)
+    {
+        Console.WriteLine($"\nTipo de contrato indefinido: {esTipoContratoIndefinidoTask.Result}");
+    }
+    else if (tareaFinalizada == sueldoNetoTask)
+    {
+        Console.WriteLine($"\nSueldo netro obtenido: {sueldoNetoTask.Result}€");
+    }
+    else if (tareaFinalizada == gastosMenusalesTask)
+    {
+        Console.WriteLine($"\nGastos mensuales obtenidos: {gastosMenusalesTask.Result}€");
+    }
+
+    analisisHipotecaTasks.Remove(tareaFinalizada); // Eliminamos de la lista de tareas para vaciar la lista y salir del while
+}
+
+
+var hipotecaAsyncConcedida = CalculadoraHipotecaAsync.AnalizarInformacionParaConcederHipoteca(aniosVidaLaboralTask.Result, esTipoContratoIndefinidoTask.Result, sueldoNetoTask.Result, gastosMenusalesTask.Result, cantidadSolicitada: 5000, aniosPagar: 30);
+
+var resultadoAsync = hipotecaAsyncConcedida ? "APROBADA" : "DENEGADA";
+
+Console.WriteLine($"\nAnalisis finalizado. Su solicitud de hipoteca ha sido: {resultadoAsync}");
+
+// Paramos el contador
+stopwatch.Stop();
+
+Console.WriteLine($"\n La operacion asincrona ha durado: {stopwatch.Elapsed}");
+
+Console.Read();
